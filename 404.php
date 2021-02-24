b55f448076180f8f85914c37cd2ae4a95b67ab77
rrsarq <25100491+rrsarq@users.noreply.github.com>
2021-02-24 18:46:54 -0300

    Criado 404.php Ajustado htaccess

---
+++ 404.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <title>Não encontrado | rrs.arq</title>
+    
+    <meta name="description" content="rrs.arq: arquitetura + engenharia de segurança + tecnologia">
+    <meta name="keywords"  content="arquitetura, engenharia, segurança, ergonomia, tecnologia">
+
+<?php include 'includes/head.php' ?>
+    
+</head>
+<body>
+    
+<?php include 'includes/header.php' ?>
+
+<main>
+    <article class="principal">
+    <h1>Nada encontrado</h1>
+    <p>O conteúdo que você procurava não foi encontrado.</p>
+    <p>Aguarde para ser redirecionado à página inicial, ou <a href="index.php">acesse neste link.</a></p>
+    </article>
+</main>
+
+<?php include 'includes/footer.php' ?>
+
+<script type="text/javascript"> 
+
+    window.setTimeout ( function() {
+        window.location.replace("https://rrs.arq.br");
+    }, 5000); 
+
+</script>
+</body>
+</html>
